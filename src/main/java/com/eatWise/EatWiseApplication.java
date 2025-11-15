5df16a378c5343754b6e2ff376615ed51741569b
Cabiyev Cavid <Cavid.Cabiyev@kapitalbank.az>
2025-11-16 00:36:51 +0400

    init commit

---
+++ src/main/java/com/eatWise/EatWiseApplication.java
@@ -0,0 +1,13 @@
+package com.eatWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eatWise/EatWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
