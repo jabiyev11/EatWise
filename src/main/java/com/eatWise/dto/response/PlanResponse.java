7574fa568b42ae4ebbbc6e46b0385baa29243c69
smehraliyev404 <saidmehraliyev@gmail.com>
2025-12-06 11:42:17 +0400

    add PlanResponse and PlanRepository

---
+++ src/main/java/com/eatWise/dto/response/PlanResponse.java
@@ -0,0 +1,18 @@
+package com.eatWise.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlanResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+}
